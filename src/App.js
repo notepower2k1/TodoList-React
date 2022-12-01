caaa43bd3508ed4d44994d4b8888cdd4c508737c
notepower2k1 <nguyenthach617@gmail.com>
2022-12-01 23:23:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoList from "./components/TodoList";
+import Textfield from '@atlaskit/textfield';
+import Button from '@atlaskit/button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <TodoList/>
+  
 }
 
 export default App;
